5ec91648bc391eb8c8377de2baa90117dce0ab6c
Diego Brum <37202217+diebrum@users.noreply.github.com>
2023-04-13 20:20:34 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ def add_numbers(a, b):
     return a + b
 
 def test_add_numbers():
-    assert add_numbers(2, 3) == 5
+    assert add_numbers(2, 3) == 6
     assert add_numbers(0, 0) == 0
     assert add_numbers(-1, 1) == 0
 
